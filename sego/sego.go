b781626dbd3aec27fbbeb5a4cce759e1fd5a109d
WindomZ <git.windomz@gmail.com>
2018-02-02 11:59:00 +0800

    feat: sego

---
+++ sego/sego.go
@@ -0,0 +1,52 @@
+package sego
+
+import (
+	"os"
+	"path"
+
+	"github.com/WindomZ/gcws"
+	"github.com/huichen/sego"
+)
+
+// Segmenter is sego.Segmenter adapter.
+type Segmenter struct {
+	Mode gcws.ModeType
+	sego.Segmenter
+}
+
+// NewSegmenter create new sego.Segmenter with paths.
+func NewSegmenter(paths ...string) gcws.CWS {
+	var filePath string
+	if len(paths) != 0 {
+		filePath = paths[0]
+	}
+	if filePath == "" {
+		filePath = path.Join(os.Getenv("GOPATH"),
+			"src/github.com/huichen/sego/data/dictionary.txt")
+	}
+	segmenter := new(Segmenter)
+	segmenter.LoadDictionary(filePath)
+	return segmenter
+}
+
+// SetMode set cws mode type.
+func (s *Segmenter) SetMode(typ gcws.ModeType) {
+	s.Mode = typ
+}
+
+// Tokenize returns the string array of split.
+func (s Segmenter) Tokenize(str string) []string {
+	return s.ModeTokenize(s.Mode, str)
+}
+
+// ModeTokenize returns the string array of split with cws mode type.
+func (s Segmenter) ModeTokenize(typ gcws.ModeType, str string) []string {
+	if typ == gcws.ModeNormal {
+		return sego.SegmentsToSlice(s.Segment([]byte(str)), false)
+	}
+	return sego.SegmentsToSlice(s.Segment([]byte(str)), true)
+}
+
+func init() {
+	gcws.Register("sego", NewSegmenter)
+}
---
+++ sego/sego_test.go
@@ -0,0 +1,25 @@
+package sego
+
+import (
+	"testing"
+
+	"github.com/WindomZ/gcws"
+	"github.com/WindomZ/testify/assert"
+)
+
+var demo gcws.CWS
+
+func init() {
+	demo, _ = gcws.NewCWS("sego")
+}
+
+func TestSegmenter_SetMode(t *testing.T) {
+	demo.SetMode(gcws.ModeNormal)
+}
+
+func TestSegmenter_Tokenize(t *testing.T) {
+	assert.Equal(t, []string{"喜欢", "就", "坚持", "，", "爱", "就", "别", "放弃"}, demo.Tokenize("喜欢就坚持，爱就别放弃"))
+
+	demo.SetMode(gcws.ModeSearch)
+	assert.Equal(t, []string{"喜欢", "就", "坚持", "，", "爱", "就", "别", "放弃"}, demo.Tokenize("喜欢就坚持，爱就别放弃"))
+}
