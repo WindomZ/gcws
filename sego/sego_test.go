c4b1341f25ba899d1f67b1b91f43efc2f13bbade
WindomZ <git.windomz@gmail.com>
2018-02-03 19:20:02 +0800

    fix cover

--- sego/sego_test.go
+++ sego/sego_test.go
@@ -25,3 +25,8 @@ func TestSegmenter_Tokenize(t *testing.T) {
 	assert.Equal(t, []string{"喜欢", "就", "坚持", "，", "爱", "就", "别", "放弃"},
 		demo.Tokenize("喜欢就坚持，爱就别放弃"))
 }
+
+func TestNewSegmenter(t *testing.T) {
+	demo, _ = gcws.NewCWS("sego", "")
+	TestSegmenter_Tokenize(t)
+}
